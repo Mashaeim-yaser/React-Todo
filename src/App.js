2cd265ecf9658926608b43b38b51d11f45d4f727
mashaeim-yaser <mashaeimyaser@gmail.com>
2022-09-08 13:45:19 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World<br/>This is React-Todo</h1>
+      <button className="btn btn-primary">Click Me!</button>
+      
+     </div>
+  );
+}
+
+export default App;
